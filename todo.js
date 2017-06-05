05db388418381ff94ef313aa5c21731496b33e4b
Madeline LeClair <madelineleclair@gmail.com>
2017-06-05 19:42:07 -0400

    Start building out logic for todo list

---
+++ todo.js
@@ -0,0 +1,24 @@
+
+export const add_listeners = () => {
+
+};
+
+const add_todo = () => {
+
+};
+
+// class Todo {
+//   constructor() {
+//     add_listeners();
+//   }
+//   add_listeners() {
+//
+//   }
+//
+//   add_todo() {
+//
+//   }
+// }
+
+
+// export default Todo;
