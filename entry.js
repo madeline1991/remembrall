41ffbc44aef337b94b8fae56d6dfb56425260776
Madeline LeClair <madelineleclair@gmail.com>
2017-06-08 19:22:05 -0400

    Make add function for todo list

--- entry.js
+++ entry.js
@@ -1,5 +1,6 @@
 // import Todo from './todo';
+import { add_listeners } from "./todo"
 
 document.addEventListener("DOMContentLoaded", () => {
-
+  add_listeners();  
 });
--- todo.js
+++ todo.js
@@ -1,24 +1,12 @@
 
 export const add_listeners = () => {
-
+  $("#submit").click(()=> add_todo());
 };
 
 const add_todo = () => {
-
+  const $title = $("#todo-title");
+  const $todoList = $("#todo-list");
+  const newTodo = `<li>${$title.val()}</li>`;
+  $todoList.append(newTodo);
+  $title.val("");
 };
-
-// class Todo {
-//   constructor() {
-//     add_listeners();
-//   }
-//   add_listeners() {
-//
-//   }
-//
-//   add_todo() {
-//
-//   }
-// }
-
-
-// export default Todo;
