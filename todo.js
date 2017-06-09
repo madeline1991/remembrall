97be970a260f5cafe859d6d6914728fab1175b18
Madeline LeClair <madelineleclair@gmail.com>
2017-06-08 20:40:57 -0400

    Start styling list items

--- todo.js
+++ todo.js
@@ -6,7 +6,10 @@ export const add_listeners = () => {
 const add_todo = () => {
   const $title = $("#todo-title");
   const $todoList = $("#todo-list");
-  const newTodo = `<li>${$title.val()}</li>`;
-  $todoList.append(newTodo);
-  $title.val("");
+  const todoTitle = $title.val();
+  const newTodo = `<li>${todoTitle}</li>`;
+  if (todoTitle != "") {
+    $todoList.append(newTodo);
+    $title.val("");
+  };
 };
