1c58c974cebc2b164ec4146689d2d7195e104c8c
Madeline LeClair <madelineleclair@gmail.com>
2017-06-12 17:08:29 -0400

    Add drag and drop function from completed todo list to incompleted todo list

--- todo.js
+++ todo.js
@@ -20,53 +20,21 @@ const delete_todo = (e) => {
 };
 
 export function dragstart_handler(ev) {
-  console.log("drag_start")
-  debugger
- // Add the target element's id to the data transfer object
  ev.dataTransfer.setData("text/html", ev.target.id);
  ev.dropEffect = "move";
 }
 
 export function dragover_handler(ev) {
-  console.log("dragover")
  ev.preventDefault();
- // Set the dropEffect to move
- ev.dataTransfer.dropEffect = "move"
+ ev.dataTransfer.dropEffect = "move";
 }
 
 export function drop_handler(ev) {
-  console.log("drop")
  ev.preventDefault();
- // Get the id of the target and add the moved element to the target's DOM
  var data = ev.dataTransfer.getData("text/html");
- ev.target.appendChild(document.getElementById(data));
+ if ($(ev.currentTarget).hasClass('complete')) {
+   $("#completed-todo").append(document.getElementById(data));
+ } else if ($(ev.currentTarget).hasClass('incomplete')) {
+   $("#todo-list").append(document.getElementById(data));
+ }
 }
-
-// export const drag = (ev) => {
-//  console.log("dragStart");
-//  // Add the target element's id to the data transfer object
-//  ev.dataTransfer.setData("text/plain", ev.target.id);
-//  ev.dropEffect = "move";
-// }
-
-
-//from w3 schools
-
-// export const drag = (e) => {
-//   debugger
-//   e.dataTransfer.setData("text", e.target.id);
-// };
-
-// export const dragover_handler = (ev) => {
-//   debugger
-//  ev.preventDefault();
-//  // Set the dropEffect to move
-//  ev.dataTransfer.dropEffect = "move"
-// }
-//
-// export const drop = (e) => {
-//   e.preventDefault();
-//   debugger
-//   var data = e.dataTransfer.getData("text");
-//   e.target.appendChild(document.getElementById(data));
-// }
